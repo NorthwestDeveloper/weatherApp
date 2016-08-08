0dcbec0b54054b06f6568da69c98d1eb090694da
Admin <Admin@Admins-MacBook.gci.net>
2016-08-07 21:28:22 -0800

    Added IBOutlets and IBAction

--- weatherApp/ViewController.swift
+++ weatherApp/ViewController.swift
@@ -10,6 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var cityTextField: UITextField!
+    
+    @IBOutlet var resultLabel: UILabel!
+    
+    
+    
+    @IBAction func getWeather(_ sender: AnyObject) {
+    
+    
+    
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
