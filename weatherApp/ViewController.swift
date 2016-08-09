f48b39b8616e188f776d97ee83b89362a823bac3
Admin <Admin@Admins-MacBook.gci.net>
2016-08-08 22:42:18 -0800

    Added code for retrieving url string and added methods to return the keyboard. Edited the UI returnLabel. TestedApp.

--- weatherApp/ViewController.swift
+++ weatherApp/ViewController.swift
@@ -8,57 +8,98 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
-    
     @IBOutlet var cityTextField: UITextField!
-    
+   
     @IBOutlet var resultLabel: UILabel!
-    
-    
-    
+
     @IBAction func getWeather(_ sender: AnyObject) {
     
+        if let url = URL(string: "http://www.weather-forecast.com/locations/" + cityTextField.text!.replacingOccurrences(of: " ", with: "-") + "/forecasts/latest") {
+        
+            let request = NSMutableURLRequest(url: url)
+        
+            let task = URLSession.shared.dataTask(with: request as URLRequest) {
+            
+                data, response, error in
+            
+                var message = ""
+            
+                if error != nil {
+                
+                    print("There was an error")
+            
+                } else {
+                
+                    if let unwrappedData = data {
+                    
+                        let dataString = NSString(data: unwrappedData, encoding: String.Encoding.utf8.rawValue)
+                    
+                        var stringSeparator = "Weather Forecast Summary:</b><span class=\"read-more-small\"><span class=\"read-more-content\"> <span class=\"phrase\">"
+                    
+                        if let contentArray = dataString?.components(separatedBy: stringSeparator) {
+                        
+                            if contentArray.count > 1 {
+                            
+                                stringSeparator = "</span>"
+                            
+                                let newContentArray = contentArray[1].components(separatedBy: stringSeparator)
+                            
+                                if newContentArray.count > 1 {
+                                
+                                message = newContentArray[0].replacingOccurrences(of: "&deg;", with: "°")
+                                
+                                print(message)
+                            
+                            }
+                            
+                        }
+                        
+                    }
+                    
+                }
+                
+            }
+            
+            if message == "" {
+                
+                message = "The weather at that location could not be found. Please try again."
+                
+            }
+            
+            DispatchQueue.main.sync(execute: {
+                
+                self.resultLabel.text = message
+                
+            })
+        }
+        
+        task.resume()
+        
+        } else {
+            
+            resultLabel.text = "Please enter a different city. The weather at that location could not be found."
+            
+        }
+    }
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        self.view.endEditing(true)
+    }
     
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        textField.resignFirstResponder()
+        
+        return true
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    
-        let url = URL(string: "http://www.weather-forecast.com/locations/New-York/forecasts/latest")!
         
-        let request = NSMutableURLRequest(url: url)
-    
-        let task = URLSession.shared.dataTask(with: request as URLRequest) {
-            data, response, error in
-            
-            if error != nil {
-                
-                print("Test this is an error test message")
-            
-            } else {
-                
-                if let unwrappedData = data {
-                    
-                    let dataString = NSString(data: unwrappedData, encoding: String.Encoding.utf8.rawValue)
-                
-                    print(dataString)
-                }
-                
-                
-            }
-        
-        
-        
-        
-        
-        
-        }
-    
-        task.resume()
-    
     }
 
     override func didReceiveMemoryWarning() {
@@ -66,6 +107,4 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
 }
-
